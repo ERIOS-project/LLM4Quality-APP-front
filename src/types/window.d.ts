87a26ebcbe345e3388a4333938ffbe6fdfbddabd
romain-mezghenna <romain.mezghenna@etu.umontpellier.fr>
2025-02-13 00:40:35 +0100

    typing fix

---
+++ src/types/window.d.ts
@@ -0,0 +1,9 @@
+// src/types/window.d.ts
+interface Window {
+  env: {
+    VITE_API_URL: string;
+    VITE_AZURE_APP_ID: string;
+    VITE_AZURE_LOCATAIRE_ID: string;
+    VITE_AZURE_API_APP_ID: string;
+  };
+}
