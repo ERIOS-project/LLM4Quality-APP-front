9ccbeb3ac83d86506aee38b5c5a5ac7ac9a28f87
romain-mezghenna <romain.mezghenna@etu.umontpellier.fr>
2025-02-13 00:20:30 +0100

    fix script for env vars

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,22 @@
 #!/bin/sh
 
-# Remplacer les variables dans index.html
+# Loop through all index.html files and replace environment variables with their values
 for file in /usr/share/nginx/html/index.html; do
   if [ -f "$file" ]; then
     echo "Injecting environment variables into $file..."
-    envsubst '\$VITE_AZURE_APP_ID \$VITE_AZURE_LOCATAIRE_ID \$VITE_AZURE_API_APP_ID \$VITE_API_URL' < "$file" > "$file.tmp" \
-      && mv "$file.tmp" "$file"
+
+    # Use envsubst to replace environment variables in the format you need
+    envsubst '
+      \$VITE_API_URL
+      \$VITE_AZURE_APP_ID
+      \$VITE_AZURE_LOCATAIRE_ID
+      \$VITE_AZURE_API_APP_ID
+    ' < "$file" > "$file.tmp" && mv "$file.tmp" "$file"
+
+    # Inject environment variables into window.env
+    sed -i 's|<\/head>|<script>window.env = { VITE_API_URL: "'$VITE_API_URL'", VITE_AZURE_APP_ID: "'$VITE_AZURE_APP_ID'", VITE_AZURE_LOCATAIRE_ID: "'$VITE_AZURE_LOCATAIRE_ID'", VITE_AZURE_API_APP_ID: "'$VITE_AZURE_API_APP_ID'" };</script></head>|' "$file"
   fi
 done
 
+# Execute the passed command (start nginx)
 exec "$@"
